09e7b5024395510df8cb2f8fa57b71c529866181
goose-in-ranch <80707474+goose-in-ranch@users.noreply.github.com>
2023-01-26 23:29:09 -0600

    Add files via upload

---
+++ rmspace.cpp
@@ -0,0 +1,43 @@
+//version 1.1
+#include <cstdio>
+#include <cstring>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+
+	if(argc < 2)
+	{
+		puts("You must supply at least one filename");
+	}
+
+	else
+	{
+		for(int i = 1; i < argc; i++) //loop args
+		{
+			int nameLen = strlen(argv[i]);
+			char oldName[nameLen] = "";
+			char newName[nameLen] = "";
+
+			strcpy(oldName, argv[i]); //fill oldname with filename placed in args
+
+			for(int j = 0; j < nameLen; j++) //loop letters in working arg
+			{
+				if(oldName[j] == ' ') strcat(newName, "-"); //if current char is space, replace with '-'
+				else strncat(newName, &oldName[j], 1); //else ignore and copy char to new filename
+			}
+
+		int result = rename(oldName, newName); //rename file and store return value in result for error checking
+			//If rename fails, create and print an error message
+			if(result != 0){
+			char errorMessage[28+strlen(oldName)] = "Oops.. Error renaming file: ";
+			strcat(errorMessage, oldName);
+			perror(errorMessage);
+			}
+//		puts(newName); //for testing
+		}
+
+	}
+
+return 0;
+}
