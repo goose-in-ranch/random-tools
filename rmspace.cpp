768b42d8287374df9882c00c564efdebab62ed82
goose-in-ranch <80707474+goose-in-ranch@users.noreply.github.com>
2023-04-25 23:19:27 -0500

    Updated rmspace.cpp
    switched previously used static arrays to dynamic arrays to prevent segfaults

--- rmspace.cpp
+++ rmspace.cpp
@@ -1,10 +1,13 @@
-//version 1.1
+//version 1.1.1
 //This is a program to remove the spaces from filenames and replace it
 //with a '-' character. (i.e. "foo bar" to "foo-bar").
 //It is called like `rmspace "file name" "foo bar"` and accepts multiple filenames.
 
 //TODO: add an option (through command like flag i.e. -s) to allow
 //a character besides - to be chosen. (- will still be the default)
+
+//TODO: There might be a problem with shell input looking like (foo\ bar) instead of ("foo bar").
+//please look into and fix.
 
 #include <cstdio>
 #include <cstring>
@@ -23,8 +26,8 @@ int main(int argc, char *argv[])
 		for(int i = 1; i < argc; i++) //loop args
 		{
 			int nameLen = strlen(argv[i]);
-			char oldName[nameLen] = "";
-			char newName[nameLen] = "";
+			char* oldName = new char[nameLen];
+			char* newName = new char[nameLen];
 
 			strcpy(oldName, argv[i]); //fill oldname with filename placed in args
 
@@ -35,6 +38,11 @@ int main(int argc, char *argv[])
 			}
 
 		int result = rename(oldName, newName); //rename file and store return value in result for error checking
+			
+			//Done with arrays, now deleting.
+			delete[] oldName;
+			delete[] newName;
+
 			//If rename fails, create and print an error message
 			if(result != 0){
 			char errorMessage[28+strlen(oldName)] = "Oops.. Error renaming file: ";
